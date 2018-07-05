886f8bc26f0452bee0262976ee550e9fdcd10d35
KAI ZHAO <kaiz.xyz@gmail.com>
2018-07-05 20:14:04 +0800

    cosine annealing schedule: a draft

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -3,6 +3,38 @@ import numpy as np
 from PIL import Image
 from os.path import isfile
 from scipy.io import savemat
+from torch.optim.lr_scheduler import _LRScheduler
+import math
+
+class CosineAnnelingLR(_LRScheduler):
+  def __init__(self, optimizer, min_lr=0.01, max_lr=0.1, cycle_length=250, cycle_length_decay=2,
+               cycle_magnitude_decay=0.5, last_epoch=-1):
+    self.max_lr = max_lr # unreferenced
+    self.min_lr = min_lr
+    self.cycle_length = cycle_length
+    self.cycle_length_decay = cycle_length_decay
+    self.cycle_magnitude_decay = cycle_magnitude_decay
+    # initiate
+    self.iter = -1
+    self.cycle = 0
+    self.base = float(1.0)
+    self.current_cycle_length = self.cycle_length
+    super(CosineAnnelingLR, self).__init__(optimizer, last_epoch)
+  def step(self, epoch=None):
+    self.iter = self.iter + 1
+    if self.iter == self.current_cycle_length:
+      self.iter = 0
+      self.current_cycle_length = int(self.current_cycle_length * self.cycle_length_decay)
+      self.base = self.base * self.cycle_magnitude_decay
+    if epoch is None:
+      epoch = self.last_epoch + 1
+    self.last_epoch = epoch
+    for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+      param_group['lr'] = lr
+  def get_lr(self):
+    return [self.min_lr + (base_lr * self.base - self.min_lr) *
+           (1 + math.cos(math.pi * self.iter / self.current_cycle_length)) / 2
+           for base_lr in self.base_lrs]
 
 def test_lfw(model, imglist, test_transform, cache_fn):
   model.eval() # switch to evaluate mode
