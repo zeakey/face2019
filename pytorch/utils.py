a2fd71b01ee6edfe999791af810267489a2435e5
KAI ZHAO <kaiz.xyz@gmail.com>
2018-07-22 10:26:51 +0800

    logging to file

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from PIL import Image
+import os, sys
 from os.path import isfile
 from scipy.io import savemat
 from torch.optim.lr_scheduler import _LRScheduler
@@ -104,3 +105,34 @@ def str2bool(x):
   else:
     raise ValueError("Invalid bool value %s" % x)
 
+class Logger(object):
+  def __init__(self, fpath=None):
+    self.console = sys.stdout
+    self.file = None
+    if fpath is not None:
+      self.file = open(fpath, 'w')
+
+  def __del__(self):
+    self.close()
+
+  def __enter__(self):
+    pass
+
+  def __exit__(self, *args):
+    self.close()
+
+  def write(self, msg):
+    self.console.write(msg)
+    if self.file is not None:
+        self.file.write(msg)
+
+  def flush(self):
+    self.console.flush()
+    if self.file is not None:
+        self.file.flush()
+        os.fsync(self.file.fileno())
+
+  def close(self):
+    self.console.close()
+    if self.file is not None:
+        self.file.close()
